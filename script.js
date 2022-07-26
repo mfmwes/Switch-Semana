8bee90f5464dbaeb03830d2ce2f0d76589c216e7
Wesley Militão <wesmilitao@gmail.com>
2022-07-26 10:58:21 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,29 @@
+let dia = 'SÁBADO'
+
+switch (dia.toLowerCase()) {
+    case 'segunda':
+        console.log('Segunda é paia')
+        break;
+    case 'terça':
+        console.log('Terça é dia de aula')
+        break;
+    case 'quarta':
+        console.log('É quarta feira meus bacanas')
+        break;
+    case 'quinta':
+        console.log('Quinta é dia de feira')
+        break;
+    case 'sexta':
+        console.log('SEXTOU !!!')
+        break;
+    case 'sábado':
+        console.log('Sábado é dia de relaxar')
+        break;
+    case 'domingo':
+        console.log('Domingo é melancólico')
+        break;
+    default:
+        break;
+}
+
+
